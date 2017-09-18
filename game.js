b04f0b7eba4b87a55d4e48c27e762f1cebd740d4
kha.peter.k <kha.peter.k@gitlab.com>
2017-09-18 18:52:04 -0400

    create drawFrame function, but drawHero won't work

--- game.js
+++ game.js
@@ -25,3 +25,8 @@ function drawBackground() {
   app.context.fillStyle = "black";
   app.context.fillRect(0, 0, app.canvas.width, app.canvas.height);
 }
+
+function drawFrame() {
+  drawBackground();
+  drawHero(hero);
+}
