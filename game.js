f35142d2810ba04a4e7ca826f8d63af4c98f1262
kha.peter.k <kha.peter.k@gitlab.com>
2017-09-18 18:24:46 -0400

    get context and draw a black background

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
 function startGame() {
   var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 }
