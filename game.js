fb7a44f01bf4ca2863463dd4468af327dbecb0e4
kha.peter.k <kha.peter.k@gitlab.com>
2017-09-18 18:36:35 -0400

    refactor drawBackground, made easier by putting everything in app object

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ function startGame() {
   app.canvas = document.getElementById("canvas");
   app.context = app.canvas.getContext("2d");
 
+  drawBackground();
+}
+
+function drawBackground() {
   app.context.fillStyle = "black";
   app.context.fillRect(0, 0, app.canvas.width, app.canvas.height);
 }
